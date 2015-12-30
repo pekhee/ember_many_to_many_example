38bbe193c11f4eb0945a80c3282efbcb6c6f3080
Pooyan Khosravi <pekhee@gmail.com>
2015-12-31 07:32:30 +0800

    mini and link behaviours

--- app/components/tags-show.js
+++ app/components/tags-show.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  init: function() {
+    this._super();
+    this.set("mini", this.attrs.mini || false);
+  },
+
+  shouldLink: function() {
+    if(this.get("link") == null) {
+      return !this.get("mini")
+    }
+
+    return this.get("link");
+  }.property("mini", "link"),
+
+  shouldExpand: Ember.computed.not("mini")
 });
