067bdc92f9f49679f67b06d0b0ed814b97c48b84
Pooyan Khosravi <pekhee@gmail.com>
2015-12-31 06:57:08 +0800

    opt in to ember data 2 behaviour
    return immediately reload at background

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,9 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
   namespace: "api",
   shouldReloadAll: function() {
+    return false;
+  },
+  shouldBackgroundReloadRecord: function() {
     return true;
   }
 });
