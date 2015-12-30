3e055fce353b1264cacfc0e211dbed9052207139
Pooyan Khosravi <pekhee@gmail.com>
2015-12-31 07:08:42 +0800

    Use new CRUD.

--- app/router.js
+++ app/router.js
@@ -5,7 +5,17 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
+var resources = function(router, resourceName) {
+  router.route(resourceName, function() {
+    this.route("create", {path: "new"});
+    this.route("show",   {path: ":id"});
+    this.route("update", {path: ":id/edit"});
+    this.route("delete", {path: ":id/delete"});
+  });
+};
+
 Router.map(function() {
+  resources(this, "posts");
 });
 
 export default Router;
